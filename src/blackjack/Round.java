f6c2d917d35db80796f84c3026167a32a359489f
pierrerispal <stageinfo@WINDOWSDEVSTAGE.DOMD1232.com>
2016-01-26 14:46:28 +0100

    commit de quelques commentaires

--- src/blackjack/Round.java
+++ src/blackjack/Round.java
@@ -17,14 +17,21 @@ public class Round {
         this.player=new Player();
         this.random=new Random();
     }
-    /*
-    * @TODO Change for android adaptation
-    */
+    /**
+     * the player (or @TODO players) got to bet at first
+     * @TODO Change for android adaptation
+     */
     public void startingBet(){
         System.out.print("Bet : ");
         String str = sc.nextLine();
         this.player.bet(Double.valueOf(str));
     }
+    /**
+     * fill the player and banquer's hand
+     * @TODO maybe a collection of players and for each one give 2 cards
+     * can be interesting to generalize the operations in case of multiples
+     * players
+     */
     public void startingCards(){
         //Hand generation
         int randomInt = random.nextInt(deck.getList().size());
@@ -37,9 +44,18 @@ public class Round {
         banquer.addCard(deck.getList().get(randomInt3));
         banquer.addCard(deck.getList().get(randomInt4));        
     }
+    /**
+     * Checking if the banquer's first card is an As
+     * @return boolean
+     */
     public boolean insuranceCheck(){
         return (banquer.getHand().get(0).getValue()==1);
     }
+    /**
+     * Checking if the player has a blackjack at the beggining
+     * @TODO maybe an abstract class for player and banquer for common methods
+     * @return boolean
+     */
     public boolean blackjackCheckPlayer(){
         return true;
     }
